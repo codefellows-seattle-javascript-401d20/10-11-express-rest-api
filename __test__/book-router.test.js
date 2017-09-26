dfc3b032456f11bfccfce5457355f09045fd6af9
KatherineHanson <katherinemariehanson@gmail.com>
2017-09-26 16:54:14 -0700

    POST, ADD, DELETE routes done

--- __test__/book-router.test.js
+++ __test__/book-router.test.js
@@ -14,10 +14,14 @@ const apiURL = `http://localhost:${process.env.PORT}`;
 const bookMockCreate = () => {
   return new Book({
     title: faker.lorem.words(10),
-    author: faker.lorem.words(10),
+    author: faker.name.findName(),
     description: faker.lorem.words(100),
     publisher: faker.lorem.words(10),
   }).save();
+};
+
+let mockManyBooks = (num) => {
+  return Promise.all(new Array(num).fill(0).map(() => bookMockCreate()));
 };
 
 describe('/api/books', () => {
@@ -31,7 +35,7 @@ describe('/api/books', () => {
     test('should respond with a book and 200 status', () => {
       let tempBook = {
         title: faker.lorem.words(10),
-        author: faker.lorem.words(10),
+        author: faker.name.findName(),
         description: faker.lorem.words(100),
         publisher: faker.lorem.words(10),
       };
@@ -63,7 +67,7 @@ describe('/api/books', () => {
     });
   });
 
-  describe('GET /api/books', () => {
+  describe('GET /api/books/:id', () => {
     // GET: test 200, it should contain a response body
     // for a request made with a valid id
     test('should respond with a book and 200 status', () => {
@@ -75,7 +79,7 @@ describe('/api/books', () => {
         })
         .then(res => {
           expect(res.status).toEqual(200);
-          expect(res.body._id).toBeTruthy();
+          expect(res.body._id).toEqual(tempBook._id.toString());
           expect(res.body.title).toEqual(tempBook.title);
           expect(res.body.author).toEqual(tempBook.author);
           expect(res.body.description).toEqual(tempBook.description);
@@ -86,10 +90,46 @@ describe('/api/books', () => {
     // GET: test 404, it should respond with 'not found'
     // for valid requests made with an id that was not found
     test('should respond with 404 status', () => {
-      return superagent.get(`${apiURL}/api/books/helloworld`)
+      return superagent.get(`${apiURL}/api/notes/hihihi`)
         .then(Promise.reject)
         .catch(res => {
           expect(res.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('DELETE /api/books/:id', () => {
+    test('should respond with a 204', () => {
+      // put data in db to delete
+      return bookMockCreate()
+        .then(book => {
+          return superagent.delete(`${apiURL}/api/books/${book._id}`);
+        })
+        .then(res => {
+          expect(res.status).toEqual(204);
+        });
+    });
+
+    test('should respond with a 404', () => {
+      return superagent.delete(`${apiURL}/api/notes/hahaha`)
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('GET /api/books', () => {
+    test('should return 100 books', () => {
+      return mockManyBooks(1000)
+        .then(tempBooks => {
+          return superagent.get(`${apiURL}/api/books`);
+        })
+        .then(res => {
+          console.log(res.headers);
+          expect(res.status).toEqual(200);
+          expect(res.body.count).toEqual(1000);
+          expect(res.body.data.length).toEqual(100);
         });
     });
   });
--- route/book-router.js
+++ route/book-router.js
@@ -1,27 +1,28 @@
 'use strict';
 
+// npm dependencies
 const {Router} = require('express');
+const httpErrors = require('http-errors');
 const jsonParser = require('body-parser').json();
 
+// app dependencies
 const Book = require('../model/book.js');
+// module interface
 const bookRouter = module.exports = new Router();
 
 // POST /api/<resource-name>
 // pass data as stringifed JSON in the body of a POST request to create a new resource
-// on success respond with a 200 status code and the created note
+// on success respond with a 200 status code and the created book
 // on failure due to a bad request send a 400 status code
 bookRouter.post('/api/books', jsonParser, (req, res, next) => {
   // create a book using the parsed body
   // and respond to the client
   if(!req.body.title || !req.body.author || !req.body.description)
-    return res.sendStatus(400);
+    next(httpErrors(400, 'title, author, and description are required'));
 
   new Book(req.body).save()
     .then(book => res.json(book))
-    .catch(err => {
-      console.error('__SERVER_ERROR__', err);
-      res.sendStatus(500);
-    });
+    .catch(next);
 });
 
 // GET /api/<resource-name> and GET /api/<resource-name>?id={id}
@@ -29,27 +30,54 @@ bookRouter.post('/api/books', jsonParser, (req, res, next) => {
 // with an id in the query string it should respond with the details of a specifc resource (as JSON)
 // if the id is not found respond with a 404
 bookRouter.get('/api/books/:id', (req, res, next) => {
-  if (req.params.id){
-    Book.findById(req.params.id)
-      .then(book => {
-        if(!book)
-          return res.sendStatus(404);
-        res.json(book);
-      })
-      .catch(err => {
-        console.error(err);
-        if(err.message.indexOf('Cast to ObjectId failed') > -1)
-          return res.sendStatus(404);
-        res.sendStatus(500);
+  Book.findById(req.params.id)
+    .then(book => {
+      if(!book) throw httpErrors(404, 'book not found');
+      res.json(book);
+    })
+    .catch(next);
+});
+
+// DELETE /api/<resource-name?id={id}>
+// the route should delete a book with the given id
+// on success this should return a 204 status code with no content in the body
+// on failure due to lack of id in the query respond with a 400 status code
+// on failure due to a resouce with that id not existing respond with a 404 status code
+bookRouter.delete('/api/books/:id', (req, res, next) => {
+  Book.findByIdAndRemove(req.params.id)
+    .then(book => {
+      if(!book) throw httpErrors(404, 'book not found');
+      res.sendStatus(204);
+    })
+    .catch(next);
+});
+
+bookRouter.get('/api/books', (req, res, next) => {
+  let {page='0'} = req.query;
+  page = Number(page);
+  if(isNaN(page)) page = 0;
+  page = page < 0 ? 0 : page;
+
+  let booksCache;
+  Book.find({})
+    .skip(page * 100)
+    .limit(100)
+    .then(books => {
+      booksCache = books;
+      return Book.find({}).count();
+    })
+    .then(count => {
+      let result = {
+        count,
+        data: booksCache,
+      };
+
+      let lastPage = Math.floor(count / 100);
+      res.links({
+        next: `http://localhost/api/books?page=${page+1}`,
+        prev: `http://localhost/api/books?page=${page < 1 ? 0 : page - 1}`,
+        last: `http://localhost/api/books?page=${lastPage}`,
       });
-  } else {
-    // send all the books
-    // use model.find() to get array of books
-    return Book.find()
-      .then(books => res.json(books.map(book => book._id)))
-      .catch(err => {
-        console.error(err);
-        res.sendStatus(res, 500);
-      });
-  }
+      res.json(result);
+    });
 });
