359d06e65d1817140871588d603a5d052f0ad520
Ron Barrantes <ronbarrantes@gmail.com>
2017-09-26 18:34:06 -0700

    test delete done

--- lab-ron/__test__/reminder-route.test.js
+++ lab-ron/__test__/reminder-route.test.js
@@ -57,4 +57,72 @@ describe('/api/reminders', () => {
         });
     });
   });
+
+  describe('GET /api/reminders:id', () => {
+    test('should respond with a reminder and a 200', () => {
+      let tempReminder;
+      return reminderMockCreate()
+        .then(reminder => {
+          tempReminder = reminder;
+          return superagent.get(`${apiURL}/api/reminders/${reminder._id}`);
+        })
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body._id).toEqual(tempReminder._id.toString());
+          expect(res.body.timestamp).toBeTruthy();
+          expect(res.body.task).toEqual(tempReminder.task);
+          expect(res.body.priority).toEqual(tempReminder.priority);
+          expect(res.body.done).toBeFalsy();
+        });
+    });
+
+    test('should respond with a 404 status', () => {
+      return superagent.get(`${apiURL}/api/reminders/coolbeans`)
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toEqual(404);
+        });
+    });
+
+  });
+
+  describe('DELETE /api/reminders:id', () => {
+    test('should respond with a 204', () => {
+      // put data in the db to delete
+      return reminderMockCreate()
+        .then(reminder => {
+          return superagent.delete(`${apiURL}/api/reminders/${reminder._id}`);
+        })
+        .then(res => {
+          expect(res.status).toEqual(204);
+        });
+    });
+
+    test('should respond with a 404', () => {
+      // put data in the db to delete
+      return superagent.delete(`${apiURL}/api/reminders/coolbeans`)
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('PUT /api/reminders:id', () => {
+    test('should update reminder and respond with 200', () => {
+      let tempReminder;
+      return reminderMockCreate()
+        .then(reminder => {
+          tempReminder = reminder;
+          return superagent.put(`${apiURL}/api/reminders/${reminder._id}`)
+            .send({ title: 'cool beans' });
+        })
+        .then(res => {
+          expect(res.status).toEqual(200);
+          expect(res.body.title).toEqual('cool beans');
+          expect(res.body.content).toEqual(tempReminder.content);
+          expect(res.body._id).toEqual(tempReminder._id.toString());
+        });
+    });
+  });
 });
